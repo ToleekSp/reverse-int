1432df7d72200b8a5c1be71259cf229f6bbd8cb9
Toleek <tom.0.0.7@yandex.ru>
2021-08-09 22:15:20 +0300

    Task Reverse Int complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  n = String(Math.abs(n));
+
+  let reverse = '';
+
+  for(let i = n.length - 1; i >= 0; i--)
+  {
+    reverse += n[i];
+  }
+
+  return Number(reverse);
 }
